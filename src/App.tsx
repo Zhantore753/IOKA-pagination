8d5ae93780c3dfbba0a564e2a49ad85614cddb92
Zhantore Bazarbayev <753gg@mail.ru>
2024-08-12 12:47:36 +0500

    App component update and styles added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Pagination from "./components/Pagination";
 import "./App.scss";
+import { IPost } from "./types/post";
+import Post from "./components/Post";
 
 const App: React.FC = () => {
+  const [posts, setPosts] = useState<Array<IPost>>([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+
+  useEffect(() => {
+    const getPosts = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch(
+          `https://jsonplaceholder.typicode.com/posts?_page=${currentPage}&_per_page=10`
+        );
+        const data = await response.json();
+        const totalPosts = response.headers.get("x-total-count");
+        setPosts(data);
+        setLoading(false);
+        setTotalPages(Math.ceil(Number(totalPosts) / 10));
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getPosts();
+  }, [currentPage]);
+
+  const onPageChange = (page: number) => {
+    setCurrentPage(page);
+  };
+
   return (
-    <div className="app">
-      <h1>Initialization</h1>
+    <div className="container">
+      <h1>Pagination Project</h1>
+      <Pagination totalPages={totalPages} onPageChange={onPageChange} />
+      <div className="posts-container">
+        {loading ? (
+          <p>Loading...</p>
+        ) : (
+          <div className="posts">
+            {posts.map((post) => (
+              <Post key={post.id} post={post} />
+            ))}
+          </div>
+        )}
+      </div>
     </div>
   );
 };
